e4d2da50e538c874362fd0b46ff74ee60522f9cc
Andy Garcia <andygarciapc@gmail.com>
2024-03-14 17:42:34 -0700

    Input Settings and Script Work

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(0)) return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(dragOrigin - Input.mousePosition);
+        Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+        transform.Translate(move, Space.World);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,23 +5,46 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f; // Adjust this to control the player's movement speed
-    public float rotationSpeed = 10f; // Adjust this to control the player's rotation speed
+    public float gravity = 9.81f;
 
-    private Rigidbody rb;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    public Transform cam;
+
+    private CharacterController controller;
 
     void Start()
     {
-        // Get the Rigidbody component attached to the player GameObject
-        rb = GetComponent<Rigidbody>();
+        // Get the CharacterController component attached to the player GameObject
+        controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-
-        _hasAnimator = TryGetComponent(out _animator);
-
-        JumpAndGravity();
-        GroundedCheck();
         Move();
+    }
+
+    void Move()
+    {
+        // Get input from the horizontal and vertical axes (arrow keys, WASD, etc.)
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Calculate the movement direction based on input
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+        if (movement.magnitude >= 0.1f)
+        {
+            //Calculate angle to face player
+            float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            // Move the player using CharacterController
+            Vector3 movementDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            movementDir = movementDir.normalized * moveSpeed * Time.deltaTime;
+            movementDir.y -= gravity * Time.deltaTime;
+            controller.Move(movementDir);
+        }
     }
 }
